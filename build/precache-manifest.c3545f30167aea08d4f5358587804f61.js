0b7107a208cd0a71f84252036ff82116e79d8ea1
Malik <mohd.saqib0607@gmail.com>
2022-04-25 21:33:48 +0530

    reactapp

---
+++ build/precache-manifest.c3545f30167aea08d4f5358587804f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c44b0098eb37f4250fa8",
+    "url": "/static/css/main.a383a3b5.chunk.css"
+  },
+  {
+    "revision": "c44b0098eb37f4250fa8",
+    "url": "/static/js/main.c44b0098.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "c328ba1513a14e3c7feb",
+    "url": "/static/js/2.c328ba15.chunk.js"
+  },
+  {
+    "revision": "526e76a96793b81fba07",
+    "url": "/static/js/runtime~main.526e76a9.js"
+  },
+  {
+    "revision": "4bc7d01461ef3636994f9ea0a128346d",
+    "url": "/index.html"
+  }
+];
---
+++ src/MyComponents/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <center><h3>My First React development</h3></center>
+            <p>Hi! I am Saqib developer of this app using react. I made this app from the learning purpose because I wanted to check the knowledge of react. I have applied all my theoretical concept on this app and achieve real word experience.
+                Latest is for stable, semver React releases. It’s what you get when you install React from npm. This is the channel you’re already using today. Use this for all user-facing React applications.
+                Next tracks the main branch of the React source code repository. Think of these as release candidates for the next minor semver release. Use this for integration testing between React and third party projects.
+                Experimental includes experimental APIs and features that aren’t available in the stable releases. These also track the main branch, but with additional feature flags turned on. Use this to try out upcoming features before they are released.
+                All releases are published to npm, but only Latest uses semantic versioning. Prereleases (those in the Next and Experimental channels) have versions generated from a hash of their contents,  for Next and next for Experimental.
+                The only officially supported release channel for user-facing applications is Latest. Next and Experimental releases are provided for testing purposes only, and we provide no guarantees that behavior won’t change between releases. They do not follow the semver protocol that we use for releases from Latest.
+                By publishing prereleases to the same registry that we use for stable releases, we are able to take advantage of the many tools that support the npm workflow, like unpkg and CodeSandbox.
+                Latest is the channel used for stable React releases. It corresponds to the latest tag on npm. It is the recommended channel for all React apps that are shipped to real users.
+                If you’re not sure which channel you should use, it’s Latest. If you’re a React developer, this is what you’re already using.
+                Our latest major version includes out-of-the-box improvements like automatic batching, new APIs like startTransition, and streaming server-side rendering with support for Suspense.
+                React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.
+                This is my first approach to build single page application in react, it is awesome experience because react switch one app to another without loading. So Thanks for Read.
+            </p>
+        </div>
+    )
+}
